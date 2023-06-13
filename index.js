595b0f4ad8c647ad820f4ef200e307dbea53e8fb
saiful-azree <azreeayie24@gmail.com>
2023-06-13 10:09:16 +0800

    Restful APIs with CRUD operations

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+//mongoDB
+const { MongoClient} = require("mongodb");
+const uri = "mongodb://172.24.74.118:27017/"
+const  client = new MongoClient(uri)
+//express
 const express = require('express')
 var jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
-
-//LAB1
+//bcrypt
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 var hashed;
@@ -29,45 +33,49 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/hello', verifyToken, (req, res) => {
-  console.log(req.user)
-
-  res.send('Welcome!')
-})
-
-
-app.post( '/login', (req, res) => {
-  let data = req.body
-  //res.send(
-    //login(
-      //data.username,
-      //data.password
-    //)
-  //)
-
-  //Get the jwt token
-    let user = login(data.username, data.password);
-    res.send(generateToken(user))
-})
-
-//LAB1
-app.post( '/login2', (req, res) => {
-  let {username, password} = req.body;
-  //Bcrypt hash
-  bcrypt.genSalt(saltRounds, function(err, salt){
-    bcrypt.hash(password, salt, function(err, hash){
-      hashed = hash
-      console.log('hash: ',hash)
-    })
+//authorization
+app.post('/visitor', verifyToken, (req, res) => {
+  if(req.user.role == 'user'){
+    //insertOne visitor to database
+  insertOne({
+    username: req.body.username,
+    password: req.body.password,
+    name: req.body.name,
+    email: req.body.email,
+    user: req.user._id
   })
-  //Sending username and hashed password to login2 function
-  setTimeout(function(){
-    login2(username, hashed)
-  },500)
-  //login2(username, hashed);
-  res.send(req.body)
+  }
+  //console.log(req.user)
+  //res.send('Welcome!')
 })
 
+app.get('/visitor', verifyToken, (req, res) => {
+  if(req.user.role == 'security'){
+    //get all visitors from database
+    find({ date: { $eq: new Date() } })
+  }
+
+  if(req.user.role == 'user'){
+    //get own visitors from database
+    find({user: { $eq: req.user._id } })
+  }
+  //res.send('Hello World!')
+})
+
+//login
+app.post( '/login',async function (req, res) {
+  let {username, password} = req.body
+  //BCRYPT hash
+  const salt = await bcrypt.genSalt(saltRounds)
+  hashed = await bcrypt.hash(password, salt)
+  console.log(hashed)
+  //login(username,hashed)
+  res.send(req.body)
+  //Get the jwt token
+    //let user = login(data.username, data.password);
+    //res.send(generateToken(user))
+})
+//register
 app.post( '/register', (req, res) => {
   let data = req.body
   res.send(
@@ -79,6 +87,18 @@ app.post( '/register', (req, res) => {
     )
   )
 });
+//change password
+app.post('/changePassword', async function (req, res){
+  const {username, newpassword} = req.body
+  await changePassword(username, newpassword)
+  res.send(req.body)
+})
+//delete account
+app.post('/deleteAccount', async function (req, res){
+  const {username, password} = req.body
+  await deleteAccount(username, password)
+  res.send(req.body)
+})
 
 app.post( '/', (req, res) => {
   const { username, password } = req.body;
@@ -98,68 +118,86 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-//FUNCTION
+//////////FUNCTION//////////
 
-function login(username, password){
-  console.log("Someone try to login with", username, password)
-  let matched = dbUsers.find(element => element.username == username)
-  if(matched){
-      if(matched.password == password){
-          return matched
-      }
-       else{
-          return "Password not matched"
-      }
-  }else{
-      return "Username not found"
-  }
-  
+//CREATE(createListing)
+async function createListing(client, newListing){
+  const result = await client.db("lab").collection("Visitor").insertOne(newListing);
+  console.log(`New listing created with the following id: ${result.insertedId}`);
 }
+//READ(login)
+async function login(username, hashed){
+  await client.connect()
+  const result = await client.db("lab").collection("Visitor").findOne({ username: username });
 
-//LAB1
-function login2(username, hashed){
-  console.log("Someone try to login with", username, password)
-  let matched = dbUsers.find(element => element.username == username)
-  if(matched){
-    //Bcrypt password verification
-    bcrypt.compare(matched.password, hashed, function(err, result){
+  if (result) {
+    //BCRYPT verify password
+    bcrypt.compare(result.password, hashed, function(err, result){
       if(result == true){
-        console.log("Access granted usin bcrypt")
-      }
-      else{
-        console.log("Password not matched")
+        console.log("Access granted. Welcome")
+      }else{
+        console.log("Wrong password")
         console.log(result)
       }
-    })
-  }
-  else{
-    console.log("Username not found")
+    });
+  } 
+  else {
+      console.log("Username not registered")
   }
 }
-
-function register(newusername, newpassword, newname, newemail){
+//CREATE(register)
+async function register(newusername, newpassword, newname, newemail){
   //TODO: Check if username exist
-  let checked = dbUsers.find(element => element.username == newusername)
-  if(checked){
-      return "Username has been taken"
-  }
-  else{
-      dbUsers.push({
+  await client.connect()
+  const exist = await client.db("lab").collection("Visitor").findOne({username: newusername})
+  if(exist){
+      console.log("Username as been")
+  }else{
+      await createListing(client,
+        {
           username: newusername,
           password: newpassword,
           name: newname,
           email: newemail
-      })
+        }
+      );
+      console.log("Registered successfully!")
   }
-  return "Registered Successfully"
+} 
+//UPDATE(change password)
+async function changePassword(savedusername, newpassword){
+  await client.connect()
+  const exist = await client.db("lab").collection("Visitor").findOne({username: savedusername})
+  if(exist){
+    await client.db("lab").collection("Visitor").updateOne({password: newpassword}, {$set: {password: newpassword}})
+    console.log("Your password has changed successfuly.")
+  }else{
+    console.log("Username does not exist.")
+  }
+}
+//DELETE(delete account)
+async function deleteAccount(oldusername, oldpassword){
+  await client.connect()
+  const exist = await client.db("lab").collection("Visitor").findOne({username: oldusername})
+  if(exist){
+    checkpassword = await exist.oldpassword
+    if(oldpassword == checkpassword){
+      await client.db("lab").collection("Visitor").deleteOne({username: oldusername})
+      console.log("Account deleted successfully.")
+    }else{
+      console.log("Password is incorrect")
+    }
+  }else{
+    console.log("Username does not exist.")
+  }
 }
 
+//AUTHORIZATION(jwt token)
 function generateToken(userProfile){
   return jwt.sign(
     userProfile,
     'SiapaCuriDiaBerdosa',  { expiresIn: 60 * 60 });
 }
-
 function verifyToken(req, res, next){
   let header = req.headers.authorization
   console.log(header)
